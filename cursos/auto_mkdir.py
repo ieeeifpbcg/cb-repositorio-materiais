ca8dea580f2862f21b2ff6f9c6a758166088e0ce
punisher077 <arthur.mts@gmail.com>
2020-02-04 13:00:55 -0300

    adicionando json e csv de engenharia

--- cursos/auto_mkdir.py
+++ cursos/auto_mkdir.py
@@ -1,17 +1,17 @@
 import os
 import json
 
-matriz_json = open("matriz_n_acentuada.json")
+matriz_json = open("engenharia-de-computacao/matriz_n_acentuada.json")
 disciplinas = json.load(matriz_json)
 
 for i in range(0,7):
-  os.system("mkdir periodo-"+str(i))
-   
+    os.system("mkdir periodo-"+str(i))
+
 
 for disciplina in disciplinas:
-  for i in range(0,7):
-    if disciplina["id"][0] == str(i):
-      os.system("mkdir ./periodo-"+str(i)+"/"+disciplina["nome"])
+    for i in range(0,7):
+        if disciplina["id"][0] == str(i):
+            os.system("mkdir ./periodo-"+str(i)+"/"+disciplina["nome"])
       os.system("echo \"#"+disciplina["nome"]+"\" >> ./periodo-"+str(i)+"/"+disciplina["nome"]+"/readme.md")
       print("mkdir ./p"+str(i)+"/"+disciplina["nome"])
       print("echo \"#"+disciplina["nome"]+"\" >> ./p"+str(disciplina)+"/"+disciplina["nome"]+"/readme.md")
