d7b83ed701af907729ca5e64baf4d0093a6f4c08
felipefbs <felipeffbs3x@gmail.com>
2020-02-15 12:14:43 -0300

    refact: melhor legibilidade do código, remoção da função que conta o numero de periodos e padronização das matrizes de engenharia e telematica

--- data/auto_mkdir.py
+++ data/auto_mkdir.py
@@ -3,6 +3,8 @@ import json
 
 
 def leJson(arquivo):
+    # Função que lê o JSON da matriz e salva em uma lista.
+
     disciplinasJson = open(arquivo, "r")
     disciplinas = json.load(disciplinasJson)
     disciplinasJson.close()
@@ -10,33 +12,17 @@ def leJson(arquivo):
     return disciplinas
 
 
-def numeroPeriodos(disciplinas):
-    periodos = []
-    for i in disciplinas:
-        periodos.append(i["id"])
-    if len(max(periodos)) == 2:
-        return int(max(periodos)[0])
-    else:
-        return int(max(periodos)[:2])
+def criaReadmeMd(disciplina, periodo, cargaHoraria, ementa):
+    # Função que cria os readme.md de cada disciplina com seu nome, periodo, CH e ementa.
 
-
-def criaMd(disciplina, periodo, cargaHoraria, ementa):
     return (
         """
-# :mortar_board: """
-        + disciplina
-        + """
-### :date: """
-        + periodo
-        + """º Período - """
-        + cargaHoraria
-        + """ horas
+# :mortar_board: """ + disciplina + """
+### :date: """ + periodo + """º Período - """ + cargaHoraria + """ horas
 
 ### :scroll: Ementa
 
-"""
-        + ementa
-        + """
+""" + ementa + """
 
 ---
 
@@ -74,14 +60,16 @@ def criaMd(disciplina, periodo, cargaHoraria, ementa):
 - [Livro 2]()
 - [Livro 3]()
 - [Livro 4]()
-- [Livro 5]()"""
-    )
+- [Livro 5]()
+""")
 
 
-def criaPastasCurso(curso, caminhoJson):
+
+def criaPastasCurso(curso, caminhoJson, numPeriodos):
+    # Função que cria as pastas de cada disciplina na pasta correspondente ao seu periodo juntamente com seu readme.md. Só é possivel utilizar este script em sistemas Unix.
+
     disciplinas = leJson(caminhoJson)[curso]
     caminho = "../cursos/"
-    numPeriodos = numeroPeriodos(disciplinas)
 
     optativas = list(
         filter(lambda disciplina: disciplina["id"][0] == "0", disciplinas))
@@ -92,13 +80,22 @@ def criaPastasCurso(curso, caminhoJson):
     os.system("mkdir " + caminho + "optativas")
 
     for i in range(1, numPeriodos + 1):
-        os.system("mkdir " + caminho + curso + "/periodo-" + str(i) + "/")
+        os.system(
+            "mkdir "
+            + caminho
+            + curso
+            + "/periodo-"
+            + str(i)
+            + "/")
 
     for disciplina in disciplinas:
         for i in range(0, numPeriodos + 1):
             if disciplina["id"][0] == str(i):
-                md = criaMd(disciplina["nome"], disciplina["id"]
-                            [0], disciplina["ch"], "ementa")
+                md = criaReadmeMd(disciplina["nome"],
+                                  disciplina["id"][0],
+                                  disciplina["ch"],
+                                  "ementa")
+
                 os.system(
                     "mkdir "
                     + caminho
@@ -123,14 +120,18 @@ def criaPastasCurso(curso, caminhoJson):
                 )
 
     for disciplina in optativas:
-        md = criaMd(disciplina["nome"], disciplina["id"]
-                    [0], disciplina["ch"], "ementa")
+        md = criaReadmeMd(disciplina["nome"],
+                          disciplina["id"][0],
+                          disciplina["ch"],
+                          "ementa")
+
         os.system(
             "mkdir "
             + caminho
             + "optativas/"
             + disciplina["nome"]
         )
+
         os.system(
             'echo "'
             + md
@@ -142,5 +143,9 @@ def criaPastasCurso(curso, caminhoJson):
         )
 
 
-criaPastasCurso("telematica", "./matrizTelematica.json")
-#criaPastasCurso("engenharia-de-computacao", "./matrizEngComputação.json")
+criaPastasCurso(curso="telematica",
+                caminhoJson="./matrizTelematica.json", 
+                numPeriodos=6)
+criaPastasCurso(curso="engenharia-de-computacao",
+                caminhoJson="./matrizEngComputacao.json", 
+                numPeriodos=10)
