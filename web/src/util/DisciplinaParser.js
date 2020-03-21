e039d37d9761eca98a863d0ac81912f2d6bfdd12
Henrique Miranda <enrque.ns@gmail.com>
2020-03-21 10:13:30 -0300

    refactor(site): disciplina parser

--- web/src/util/DisciplinaParser.js
+++ web/src/util/DisciplinaParser.js
@@ -1,13 +1,10 @@
-module.exports = {
-  parseName(name) {
-    return name
-      .split('-')
-      .map(s => {
-        if (s.length > 2) {
-          return s.charAt(0).toUpperCase() + s.slice(1);
-        }
-        return s;
-      })
-      .join(' ');
-  },
-};
+export function parseName(name) {
+  return name
+    .split('-')
+    .map(part => {
+      if (part.length <= 2) return part;
+
+      return part.charAt(0).toUpperCase() + part.slice(1);
+    })
+    .join(' ');
+}
